e9f5ecfd4d3a95c76e1c18e5f52bb32d6c5425fc
gabygarro <ggarroab@gmail.com>
2018-11-26 11:48:46 -0600

    actualizando useername

--- plugins/about.js
+++ plugins/about.js
@@ -9,7 +9,7 @@ module.exports = function(bot) {
 
     var exec = function(msg, reply) {
         
-        reply.sendMessage("Creado por @GabyGarro, con ayuda de [lamalditalisiada.com](lamalditalisiada.com)\nSolucionando el problema de voice notes en iOS.",
+        reply.sendMessage("Creado por @hydrochaeris, con ayuda de [lamalditalisiada.com](lamalditalisiada.com)",
             {parse_mode: 'Markdown' });
     };
 
