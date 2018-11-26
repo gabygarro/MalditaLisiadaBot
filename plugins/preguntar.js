f544af23634f9da75aa8bf36a74ae71d29612c26
gabygarro <ggarroab@gmail.com>
2018-11-26 13:20:56 -0600

    cargar stciekrs y frases desde archivos

--- plugins/preguntar.js
+++ plugins/preguntar.js
@@ -3,40 +3,42 @@
 var _ = require('lodash');
 var utils = require('../lib/utils');
 
+var fs = require('fs');
+var readline = require('readline');
+var stream = require('stream');
+
+function loadFile(filename, array) {
+    var instream = fs.createReadStream('./' + filename);
+    var outstream = new stream;
+    var rl = readline.createInterface(instream, outstream);
+
+    rl.on('line', function(line) {
+        array.push(line);
+    });
+}
+
 module.exports = function (bot) {
 
     var name = "preguntar";
     var description = "[mensaje] - Me puedes hacer preguntas, pero solo si no son muy tontas";
-    var frases = [
-        'Es cierto',
-        'Sí, es así',
-        'Sin ninguna duda',
-        'Definitivamente sí',
-        'Definitivamente no',
-        'Creo que es lo más probable',
-        'Sí',
-        'No sé, pero talvez la vieja zorra esa te pueda decir',
-        'Pregúntame luego',
-        'Mejor no te lo digo ahora',
-        'No puedo predecirlo con mis poderes satánicos',
-        'No lo creo, querida',
-        '¿Quién te crees?! NO',
-        'Mis fuentes me dicen que no',
-        'Lo dudo, ilusa',
-        'No lo sé, pero esa estúpida igualada me quiere robar a mi Nandito',
-        'Pregúntale a tu dios',
-        'Más o menos',
-        'Puede ser',
-        'Sí, pero no porque la señora gata esa lo dijo'
-    ];
+    var frases = [];
+    var stickers = [];
+    loadFile('frases.txt', frases);
+    loadFile('stickers.txt', stickers);
 
     var exec = function (msg) {
         var pregunta = msg.command.params[0] || null;
         if (pregunta != null) {
             var respuesta = "";
-            respuesta = frases[_.random(frases.length - 1)];
-            bot.sendMessage(msg.chat.id, respuesta, 
-                { reply_to_message_id: msg.message_id});
+            var probability = _.random(5);
+            if(probability > 2) {
+                bot.sendSticker(msg.chat.id, stickers[_.random(stickers.length - 1)]);
+            }
+            else {
+                respuesta = frases[_.random(frases.length - 1)];
+                bot.sendMessage(msg.chat.id, respuesta, 
+                    { reply_to_message_id: msg.message_id});
+            }
         }
         else {
             respuesta = "No puedo responder si no me preguntas, igualada.\nEjemplo: /preguntar ¿Porqué eres tan marginal?";
